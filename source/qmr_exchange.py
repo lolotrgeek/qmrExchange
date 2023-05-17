d10a24a34bb7ef5e759776bfab7bfbd888903349
lolotrgeek <jon@simplyenable.com>
2023-05-17 13:39:24 -0500

    Revert "fix issue #3"
    This reverts commit 77d43e8d492f1d352a2ebbcc1d8514687c8de6d4.

--- source/qmr_exchange.py
+++ source/qmr_exchange.py
@@ -534,19 +534,17 @@ class Simulator():
 
     def get_portfolio_history(self,agent):
         agent = self.get_agent(agent)
-        bar_size =  get_pandas_time(self._time_unit)
-        portfolio = pd.DataFrame(index=get_datetime_range(self._from_date,self._to_date,self._time_unit))
+        portfolio = pd.DataFrame(index=self.datetime_range)
         transactions = pd.DataFrame(agent._transactions).set_index('dt')
+        bar_size =  get_pandas_time(self._time_unit)
         for ticker in list(self.exchange.books.keys()):
             qty_asset = pd.DataFrame(transactions[transactions['ticker']==ticker]['qty'])
             qty_asset = qty_asset.resample(bar_size).agg('sum').cumsum()
-            price = pd.DataFrame(index=get_datetime_range(self._from_date,self._to_date,self._time_unit))
-            price = price.join(self.get_price_bars(ticker=ticker,bar_size=bar_size)['close']).ffill()
-            price = price.join(qty_asset).ffill()
-            portfolio[ticker] = (price['close'] * price['qty'])
-        portfolio['cash'] = transactions['cash_flow'].resample(bar_size).agg('sum').ffill()
+            price = pd.DataFrame(self.get_price_bars(ticker=ticker,bar_size=bar_size)['close'].ffill())
+            portfolio[ticker] = (price['close'] * qty_asset['qty'])
+        portfolio['cash'] = transactions['cash_flow'].resample(bar_size).agg('sum').ffill().cumsum()
         portfolio.fillna(0,inplace=True)
-        portfolio['cash'] = portfolio['cash'].cumsum()
+        portfolio['cash'] += agent.initial_cash
         portfolio['aum'] = portfolio.sum(axis=1)
         return portfolio
 
