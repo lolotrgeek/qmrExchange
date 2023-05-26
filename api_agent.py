96d550208fef558324ba3a5640a515021e717cab
lolotrgeek <jon@simplyenable.com>
2023-05-26 07:06:37 -0500

    feat: async, still slow

--- api_agent.py
+++ api_agent.py
@@ -1,39 +1,49 @@
 from main import AgentRemote
+import asyncio
 
 remote_agent = AgentRemote("test", ["XYZ"], 10000)
-last_trade = remote_agent.get_latest_trade('XYZ')
-best_bid = remote_agent.get_best_bid('XYZ')
-best_ask = remote_agent.get_best_ask('XYZ')
-midprice = remote_agent.get_midprice('XYZ')
-order_book = remote_agent.get_order_book('XYZ')
-quotes = remote_agent.get_quotes('XYZ')
-trades = remote_agent.get_trades('XYZ')
 
-market_buy = remote_agent.market_buy('XYZ', 1)
-market_sell = remote_agent.market_sell('XYZ', 1)
-limit_buy = remote_agent.limit_buy('XYZ', 1, 1)
-limit_sell = remote_agent.limit_sell('XYZ', 1, 1)
+async def main():
+    last_trade = await remote_agent.get_latest_trade('XYZ')
+    best_bid = await remote_agent.get_best_bid('XYZ')
+    best_ask = await remote_agent.get_best_ask('XYZ')
+    midprice = await remote_agent.get_midprice('XYZ')
+    order_book = await remote_agent.get_order_book('XYZ')
+    quotes = await remote_agent.get_quotes('XYZ')
+    trades = await remote_agent.get_trades('XYZ')
 
-get_price_bars = remote_agent.get_price_bars('XYZ', '1D', 20)
+    # market_buy = remote_agent.market_buy('XYZ', 1)
+    # market_sell = remote_agent.market_sell('XYZ', 1)
+    # limit_buy = remote_agent.limit_buy('XYZ', 1, 1)
+    # limit_sell = remote_agent.limit_sell('XYZ', 1, 1)
 
-
-#TODO: need to save order_ids in remtoe_agent
-# cancel_order = remote_agent.cancel_order()
-
-#TODO: register remote agent with the exchange
-# cancel_all_orders = remote_agent.cancel_all_orders('XYZ')
+    # get_price_bars = remote_agent.get_price_bars('XYZ', '1D', 20)
 
 
-print("last_trade", last_trade)
-print("best_bid", best_bid)
-print("midprice", remote_agent.get_midprice('XYZ'))
-print("order_book", order_book)
-print("quotes", quotes)
-print("trades", trades)
-print("market_buy", market_buy)
-print("market_sell", market_sell)
-print("limit_buy", limit_buy)
-print("limit_sell", limit_sell)
-# print("cancel_order", cancel_order)
-# print("cancel_all_orders", cancel_all_orders)
-print("get_price_bars", get_price_bars)
+    #TODO: need to save order_ids in remtoe_agent
+    # cancel_order = remote_agent.cancel_order()
+
+    #TODO: register remote agent with the exchange
+    # cancel_all_orders = remote_agent.cancel_all_orders('XYZ')
+
+
+    print("last_trade", last_trade)
+    print("best_bid", best_bid)
+    print("best_ask", best_ask)
+    print("midprice", midprice)
+    print("order_book", order_book)
+    print("quotes", quotes)
+    print("trades", trades)
+
+    # print("market_buy", market_buy)
+    # print("market_sell", market_sell)
+    # print("limit_buy", limit_buy)
+    # print("limit_sell", limit_sell)
+    
+    # print("cancel_order", cancel_order)
+    # print("cancel_all_orders", cancel_all_orders)
+    
+    # print("get_price_bars", get_price_bars)
+
+# loop = asyncio.get_event_loop()
+asyncio.run(main())
--- source/backend/AgentRemote.py
+++ source/backend/AgentRemote.py
@@ -6,6 +6,8 @@ from .LimitOrder import LimitOrder
 from .OrderBook import OrderBook
 from uuid import uuid4 as UUID
 from requests import get, post
+import aiohttp
+import asyncio
 import json
 
 class AgentRemote():
@@ -26,7 +28,7 @@ class AgentRemote():
     def __str__(self):
         return f'<Agent: {self.name}>'
 
-    def get_latest_trade(self, ticker:str) -> Trade:
+    async def get_latest_trade(self, ticker:str) -> Trade:
         """Returns the most recent trade of a given asset
 
         Args:
@@ -35,12 +37,14 @@ class AgentRemote():
         Returns:
             Trade: the most recent trade
         """
-        url = f'http://localhost:5000/api/v1/get_latest_trade?ticker={ticker}'
-        response = get(url)
-        dict_response = response.json()
-        return Trade(dict_response['ticker'], dict_response['qty'], dict_response['price'], dict_response['buyer'], dict_response['seller'], dict_response['dt'], dict_response['fee'])
+        async with aiohttp.ClientSession() as session:
+            url = f'http://localhost:5000/api/v1/get_latest_trade?ticker={ticker}'
+            async with session.get(url) as response:
+                
+                dict_response = await response.json()
+                return Trade(dict_response['ticker'], dict_response['qty'], dict_response['price'], dict_response['buyer'], dict_response['seller'], dict_response['dt'], dict_response['fee'])
 
-    def get_best_bid(self, ticker:str) -> LimitOrder:
+    async def get_best_bid(self, ticker:str) -> LimitOrder:
         """Returns the current best limit buy order
 
         Args:
@@ -49,13 +53,15 @@ class AgentRemote():
         Returns:
             LimitOrder: the current best limit buy order
         """
-        url = f'http://localhost:5000/api/v1/get_best_bid?ticker={ticker}'
-        response = get(url)
-        dict_response = response.json()
-        # dict_response = json.loads(response.json())
-        return LimitOrder(dict_response['ticker'], dict_response['qty'], dict_response['price'], dict_response['creator'], dict_response['id'])
+        async with aiohttp.ClientSession() as session:
+            url = f'http://localhost:5000/api/v1/get_best_bid?ticker={ticker}'
+            async with session.get(url) as response:
+                
+                dict_response = await response.json()
+                # dict_response = json.loads(response.json())
+                return LimitOrder(dict_response['ticker'], dict_response['qty'], dict_response['price'], dict_response['creator'], dict_response['id'])
     
-    def get_best_ask(self, ticker:str) -> LimitOrder:
+    async def get_best_ask(self, ticker:str) -> LimitOrder:
         """Returns the current best limit sell order
 
         Args:
@@ -64,12 +70,14 @@ class AgentRemote():
         Returns:
             LimitOrder: the current best limit sell order
         """
-        url = f'http://localhost:5000/api/v1/get_best_ask?ticker={ticker}'
-        response = get(url)
-        dict_response = response.json()
-        return LimitOrder(dict_response['ticker'], dict_response['qty'], dict_response['price'], dict_response['creator'], dict_response['id'])
+        async with aiohttp.ClientSession() as session:
+            url = f'http://localhost:5000/api/v1/get_best_ask?ticker={ticker}'
+            async with session.get(url) as response:
+                
+                dict_response = await response.json()
+                return LimitOrder(dict_response['ticker'], dict_response['qty'], dict_response['price'], dict_response['creator'], dict_response['id'])
           
-    def get_midprice(self, ticker:str) -> float:
+    async def get_midprice(self, ticker:str) -> float:
         """Returns the current midprice of the best bid and ask orders in the orderbook of an asset
 
         Args:
@@ -78,30 +86,35 @@ class AgentRemote():
         Returns:
             float: the current midprice
         """
-        url = f'http://localhost:5000/api/v1/get_midprice?ticker={ticker}'
-        response = get(url)
-        dict_response = response.json()
-        return dict_response['midprice']
+        async with aiohttp.ClientSession() as session:
+            url = f'http://localhost:5000/api/v1/get_midprice?ticker={ticker}'
+            async with session.get(url) as response:    
+                
+                dict_response = await response.json()
+                return dict_response['midprice']
 
-    def get_order_book(self,ticker:str) -> OrderBook:
-        url = f'http://localhost:5000/api/v1/get_order_book?ticker={ticker}'
-        response = get(url)
-        dict_response = response.json()
-        return dict_response
+    async def get_order_book(self,ticker:str) -> OrderBook:
+        async with aiohttp.ClientSession() as session:
+            url = f'http://localhost:5000/api/v1/get_order_book?ticker={ticker}'
+            async with session.get(url) as response:
+                dict_response = await response.json()
+                return dict_response
 
-    def get_quotes(self,ticker):
-        url = f'http://localhost:5000/api/v1/get_quotes?ticker={ticker}'
-        response = get(url)
-        dict_response = response.json()
-        return dict_response
+    async def get_quotes(self,ticker):
+        async with aiohttp.ClientSession() as session:
+            url = f'http://localhost:5000/api/v1/get_quotes?ticker={ticker}'
+            async with session.get(url) as response:
+                dict_response = await response.json()
+                return dict_response
 
-    def get_trades(self, ticker):
-        url = f'http://localhost:5000/api/v1/get_trades?ticker={ticker}'
-        response = get(url)
-        dict_response = response.json()
-        return dict_response
+    async def get_trades(self, ticker):
+        async with aiohttp.ClientSession() as session:
+            url = f'http://localhost:5000/api/v1/get_trades?ticker={ticker}'
+            async with session.get(url) as response:
+                dict_response = await response.json()
+                return dict_response
 
-    def market_buy(self, ticker:str, qty:int, fee=0.0):
+    async def market_buy(self, ticker:str, qty:int, fee=0.0):
         """Places a market buy order. The order executes automatically at the best sell price if ask quotes are available.
 
         Args:
@@ -109,13 +122,15 @@ class AgentRemote():
             qty (int): the quantity of the asset to be acquired (in units)
 
         """
-        post_url = f'http://localhost:5000/api/v1/market_buy'
-        data = {'ticker': ticker, 'qty': qty, 'buyer': self.name, 'fee': fee}
-        response = post(post_url, json=data)
-        dict_response = response.json()
-        return dict_response
+        async with aiohttp.ClientSession() as session:
+            post_url = f'http://localhost:5000/api/v1/market_buy'
+            data = {'ticker': ticker, 'qty': qty, 'buyer': self.name, 'fee': fee}
+            async with session.post(post_url, json=data) as response:
+                response = post(post_url, data)
+                dict_response = await response.json()
+                return dict_response
     
-    def market_sell(self, ticker:str, qty:int, fee=0.0):
+    async def market_sell(self, ticker:str, qty:int, fee=0.0):
         """Places a market sell order. The order executes automatically at the best buy price if bid quotes are available.
 
         Args:
@@ -126,10 +141,10 @@ class AgentRemote():
         post_url = f'http://localhost:5000/api/v1/market_sell'
         data = {'ticker': ticker, 'qty': qty, 'seller': self.name, 'fee': fee}
         response = post(post_url, json=data)
-        dict_response = response.json()
+        dict_response = await response.json()
         return dict_response
 
-    def limit_buy(self, ticker:str, price:float, qty:int, fee=0.0) -> LimitOrder:
+    async def limit_buy(self, ticker:str, price:float, qty:int, fee=0.0) -> LimitOrder:
         """Creates a limit buy order for a given asset and quantity at a certain price.
 
         Args:
@@ -143,10 +158,10 @@ class AgentRemote():
         post_url = f'http://localhost:5000/api/v1/limit_buy'
         data = {'ticker': ticker, 'price': price, 'qty': qty, 'creator': self.name, 'fee': fee}
         response = post(post_url, json=data)
-        dict_response = response.json()
+        dict_response = await response.json()
         return LimitOrder(dict_response['ticker'], dict_response['qty'], dict_response['price'], dict_response['creator'], dict_response['id'])
 
-    def limit_sell(self, ticker:str, price:float, qty:int, fee=0.0) -> LimitOrder:
+    async def limit_sell(self, ticker:str, price:float, qty:int, fee=0.0) -> LimitOrder:
         """Creates a limit sell order for a given asset and quantity at a certain price.
 
         Args:
@@ -160,13 +175,13 @@ class AgentRemote():
         post_url = f'http://localhost:5000/api/v1/limit_sell'
         data = {'ticker': ticker, 'price': price, 'qty': qty, 'creator': self.name, 'fee': fee}
         response = post(post_url, json=data)
-        dict_response = response.json()
+        dict_response = await response.json()
         return LimitOrder(dict_response['ticker'], dict_response['qty'], dict_response['price'], dict_response['creator'], dict_response['id'])
     
-    def get_position(self,ticker):
+    async def get_position(self,ticker):
         return sum(t['qty'] for t in self._transactions if t['ticker'] == ticker)
 
-    def get_cash_history(self):
+    async def get_cash_history(self):
         return pd.DataFrame(self.__cash_history).set_index('dt')
 
     @property
@@ -175,10 +190,10 @@ class AgentRemote():
         trades = trades[trades[['buyer','seller']].isin([self.name]).any(axis=1)]
         return trades
 
-    def _set_exchange(self,exchange):
+    async def _set_exchange(self,exchange):
         self.exchange = exchange
 
-    def cancel_order(self, id:str) -> Union[LimitOrder,None]:
+    async def cancel_order(self, id:str) -> Union[LimitOrder,None]:
         """Cancels the order with a given id (if it exists)
 
         Args:
@@ -190,10 +205,10 @@ class AgentRemote():
         post_url = f'http://localhost:5000/api/v1/cancel_order'
         data = {'id': id}
         response = post(post_url, json=data)
-        dict_response = response.json()
+        dict_response = await response.json()
         return LimitOrder(dict_response['ticker'], dict_response['qty'], dict_response['price'], dict_response['creator'], dict_response['id'])
 
-    def cancel_all_orders(self, ticker:str):
+    async def cancel_all_orders(self, ticker:str):
         """Cancels all remaining orders that the agent has on an asset.
 
         Args:
@@ -202,13 +217,13 @@ class AgentRemote():
         post_url = f'http://localhost:5000/api/v1/cancel_all_orders'
         data = {'agent': self.name, 'ticker': ticker}
         response = post(post_url, json=data)
-        dict_response = response.json()
+        dict_response = await response.json()
         return dict_response
 
-    def get_price_bars(self, ticker, bar_size='1D', limit=20):
+    async def get_price_bars(self, ticker, bar_size='1D', limit=20):
         url = f'http://localhost:5000/api/v1/candles?ticker={ticker}&interval={bar_size}&limit={limit}'
-        response = get(url)
-        dict_response = response.json()
+        
+        dict_response = await response.json()
         return dict_response
     
     def next(self):  
