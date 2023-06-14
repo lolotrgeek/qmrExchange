389a2cf46f89c601bbf316ca44fafcaf08512052
lolotrgeek <jon@simplyenable.com>
2023-06-14 14:17:47 -0500

    chore: stress test the api

--- api_agent.js
+++ api_agent.js
@@ -18,19 +18,23 @@ function hammer() {
     const start = Date.now()
     let runs = 100
     let run = 0
+    let runners = []
     while (run < runs) {
         if(run >= runs) break
         run += 1
-        setTimeout(() => {
-            functions[Math.floor(Math.random() * functions.length)]()
-        }, 100)
+        runners.push(new Promise(resolve => setTimeout(() => {
+            functions[Math.floor(Math.random() * functions.length)](resolve)
+        }, 100) ) )
     }
-    const end = Date.now()
-    console.log(count, end - start)
+
+    Promise.all(runners).then(() => {
+        const end = Date.now()
+        console.log(count, end - start)
+    })
 }
 
 // make get request to api
-function make_request(url) {
+function make_request(url, resolve) {
     http.get(url, response => {
         let body = ''
         response.on('data', data => {
@@ -38,8 +42,8 @@ function make_request(url) {
         })
 
         response.on('end', () => {
-            const profile = JSON.parse(body)
-            // console.log(url, profile)
+            let response = JSON.parse(body)
+            resolve(response)
             count += 1
         })
     }
@@ -47,39 +51,39 @@ function make_request(url) {
 }
 
 
-function get_latest_trade(ticker='XYZ') {
-    const url =  `${base}/get_latest_trade?ticker=${ticker}`
-    make_request(url)
+function get_latest_trade(resolve) {
+    const url =  `${base}/get_latest_trade?ticker=XYZ`
+    make_request(url, resolve)
 }
 
-function get_best_bid(ticker='XYZ') {
-    const url =  `${base}/get_best_bid?ticker=${ticker}`
-    make_request(url)
+function get_best_bid(resolve) {
+    const url =  `${base}/get_best_bid?ticker=XYZ`
+    make_request(url, resolve)
 }
 
-function get_best_ask(ticker='XYZ') {
-    const url =  `${base}/get_best_ask?ticker=${ticker}`
-    make_request(url)
+function get_best_ask(resolve) {
+    const url =  `${base}/get_best_ask?ticker=XYZ`
+    make_request(url, resolve)
 }
 
-function get_midprice(ticker='XYZ') {
-    const url =  `${base}/get_midprice?ticker=${ticker}`
-    make_request(url)
+function get_midprice(resolve) {
+    const url =  `${base}/get_midprice?ticker=XYZ`
+    make_request(url, resolve)
 }
 
-function get_order_book(ticker='XYZ') {
-    const url =  `${base}/get_order_book?ticker=${ticker}`
-    make_request(url)
+function get_order_book(resolve) {
+    const url =  `${base}/get_order_book?ticker=XYZ`
+    make_request(url, resolve)
 }
 
-function get_quotes(ticker='XYZ') {
-    const url =  `${base}/get_quotes?ticker=${ticker}`
-    make_request(url)
+function get_quotes(resolve) {
+    const url =  `${base}/get_quotes?ticker=XYZ`
+    make_request(url, resolve)
 }
 
-function get_trades(ticker='XYZ') {
-    const url =  `${base}/get_trades?ticker=${ticker}`
-    make_request(url)
+function get_trades(resolve) {
+    const url =  `${base}/get_trades?ticker=XYZ`
+    make_request(url, resolve)
 }
 
 
