429fe17b9778c883aca47cd6f040bbd4e15199b8
lolotrgeek <jon@simplyenable.com>
2023-06-15 06:47:38 -0500

    refactor: moves portfolio to Agent

--- source/backend/AgentProcess.py
+++ source/backend/AgentProcess.py
@@ -1,3 +1,4 @@
+from .utils._utils import get_datetime_range, get_pandas_time
 import pandas as pd
 from typing import List, Union
 from .types.Trade import Trade
@@ -160,6 +161,23 @@ class Agent():
     
     def get_assets(self):
         return self.requests.get_assets(self.name)
+    
+    def get_portfolio_history(self, agent):
+        bar_size = get_pandas_time(self._time_unit)
+        portfolio = pd.DataFrame(index=get_datetime_range(self._from_date, self.dt, self._time_unit))
+        transactions = pd.DataFrame(agent._transactions).set_index('dt')
+        for ticker in list(self.exchange.books.keys()):
+            qty_asset = pd.DataFrame(transactions[transactions['ticker'] == ticker]['qty'])
+            qty_asset = qty_asset.resample(bar_size).agg('sum').cumsum()
+            price = pd.DataFrame(index=get_datetime_range(self._from_date, self.dt, self._time_unit))
+            price = price.join(self.get_price_bars(ticker=ticker, bar_size=bar_size)['close']).ffill()
+            price = price.join(qty_asset).ffill()
+            portfolio[ticker] = (price['close'] * price['qty'])
+        portfolio['cash'] = transactions['cash_flow'].resample(bar_size).agg('sum').ffill()
+        portfolio.fillna(0, inplace=True)
+        portfolio['cash'] = portfolio['cash'].cumsum()
+        portfolio['aum'] = portfolio.sum(axis=1)
+        return portfolio  
     
     def register(self):
         return self.requests.register_agent(self.name, self.initial_cash)
