ad8d658007a6eee43f4af29c92ba9cfc62dccf7a
lolotrgeek <jon@simplyenable.com>
2023-06-19 08:38:48 -0500

    feat: add fills to market order return

--- source/Exchange.py
+++ source/Exchange.py
@@ -254,6 +254,7 @@ class Exchange():
 
     def market_buy(self, ticker: str, qty: int, buyer: str, fee=0.0):
         if self.agent_has_cash(buyer, self.get_best_ask(ticker).price, qty):
+            fills = []
             for idx, ask in enumerate(self.books[ticker].asks):
                 trade_qty = min(ask.qty, qty)
                 self.books[ticker].asks[idx].qty -= trade_qty
@@ -261,16 +262,18 @@ class Exchange():
                 taker_fee = self.fees.taker_fee(qty)
                 self.fees.total_fee_revenue += taker_fee
                 if(type(fee) is str): fee = float(fee)
+                fills.append({'qty': trade_qty, 'price': ask.price, 'fee': fee+taker_fee})
                 self._process_trade(ticker, trade_qty,ask.price, buyer, ask.creator, fee=fee+taker_fee)
                 if qty == 0:
                     break
             self.books[ticker].asks = [ask for ask in self.books[ticker].asks if ask.qty > 0]
-            return {"market_buy": ticker }
+            return {"market_buy": ticker, "qty": qty, "buyer": buyer, "fee": fee, "taker_fee": taker_fee, "fills": fills}
         else:
             return {"market_buy": "insufficient funds"}
 
     def market_sell(self, ticker: str, qty: int, seller: str, fee=0.0):
         if self.agent_has_assets(seller, ticker, qty):
+            fills = []
             for idx, bid in enumerate(self.books[ticker].bids):
                 trade_qty = min(bid.qty, qty)
                 self.books[ticker].bids[idx].qty -= trade_qty
@@ -278,11 +281,12 @@ class Exchange():
                 taker_fee = self.fees.taker_fee(qty)
                 self.fees.total_fee_revenue += taker_fee
                 if(type(fee) is str): fee = float(fee)
+                fills.append({'qty': trade_qty, 'price': ask.price, 'fee': fee+taker_fee})
                 self._process_trade(ticker, trade_qty,bid.price, bid.creator, seller, fee=fee+taker_fee)
                 if qty == 0:
                     break
             self.books[ticker].bids = [bid for bid in self.books[ticker].bids if bid.qty > 0]
-            return {"market_sell": ticker}
+            return {"market_sell": ticker, "qty": qty, "seller": seller, "fee": fee, "taker_fee": taker_fee, "fills": fills }
         else:
             return {"market_sell": "insufficient assets"}
 
