25bf06df55bee95c7b09f50f16ce4b5c6fc219d3
lolotrgeek <jon@simplyenable.com>
2023-06-19 08:19:35 -0500

    feat: waive fees by default

--- source/types/Fees.py
+++ source/types/Fees.py
@@ -1,13 +1,18 @@
 
 class Fees():
     def __init__(self):
+        self.waive_fees = True
         self.maker_fee_rate = 0.001  # Maker fee rate as a decimal (e.g., 0.001 = 0.1%)
         self.taker_fee_rate = 0.002  # Taker fee rate as a decimal (e.g., 0.002 = 0.2%)
         self.total_fee_revenue = 0.0  # Total fee revenue collected by the exchange
 
     def taker_fee(self, volume):
+        if self.waive_fees:
+            return 0.0
         return volume * self.taker_fee_rate
     
     def maker_fee(self, volume):
+        if self.waive_fees:
+            return 0.0
         return volume * self.maker_fee_rate
         
