745e888a64a7be3665685c2e4139ef425c78eb0f
lolotrgeek <jon@simplyenable.com>
2023-06-28 09:14:41 -0500

    chore: reducing dash to agets and latest trade

--- source/app/dashboard/src/Table.js
+++ source/app/dashboard/src/Table.js
@@ -4,6 +4,7 @@ const base_url = 'http://127.0.0.1:5000'
 
 const TableComponent = ({ ticker }) => {
     const [agents, setAgents] = useState([])
+    const [latestTrade, setLatestTrade] = useState({})
     const [candles, setCandles] = useState([])
     const [orderBook, setOrderBook] = useState({bids:[], asks:[]})
     const [trades, setTrades] = useState([])
@@ -14,38 +15,42 @@ const TableComponent = ({ ticker }) => {
 
     useEffect(() => {
         const fetchData = async () => {
-            // const agentsResponse = await fetch(`${base_url}/api/v1/get_agents`)
-            // const agentsData = await agentsResponse.json()
-            // setAgents(agentsData)
+            const agentsResponse = await fetch(`${base_url}/api/v1/get_agents`)
+            const agentsData = await agentsResponse.json()
+            setAgents(agentsData)
+
+            const getLatestTrade = await fetch(`${base_url}/api/v1/get_latest_trade?ticker=${ticker}`)
+            const getLatestTradeData = await getLatestTrade.json()
+            setLatestTrade(getLatestTradeData)
 
             // const candleResponse = await fetch(`${base_url}/api/v1/candles?ticker=${ticker}`)
             // const candleData = await candleResponse.json()
             // console.log(candleData)
             // setCandles(candleData)
 
-            const orderBookResponse = await fetch(`${base_url}/api/v1/get_order_book?ticker=${ticker}`)
-            const orderBookData = await orderBookResponse.json()
-            setOrderBook(orderBookData)
+            // const orderBookResponse = await fetch(`${base_url}/api/v1/get_order_book?ticker=${ticker}`)
+            // const orderBookData = await orderBookResponse.json()
+            // setOrderBook(orderBookData)
 
-            const tradesResponse = await fetch(`${base_url}/api/v1/get_trades?ticker=${ticker}`)
-            const tradesData = await tradesResponse.json()
-            setTrades(tradesData)
+            // const tradesResponse = await fetch(`${base_url}/api/v1/get_trades?ticker=${ticker}`)
+            // const tradesData = await tradesResponse.json()
+            // setTrades(tradesData)
 
-            const quotesResponse = await fetch(`${base_url}/api/v1/get_quotes?ticker=${ticker}`)
-            const quotesData = await quotesResponse.json()
-            setQuotes(quotesData)
+            // const quotesResponse = await fetch(`${base_url}/api/v1/get_quotes?ticker=${ticker}`)
+            // const quotesData = await quotesResponse.json()
+            // setQuotes(quotesData)
 
-            const bestBidResponse = await fetch(`${base_url}/api/v1/get_best_bid?ticker=${ticker}`)
-            const bestBidData = await bestBidResponse.json()
-            setBestBid(bestBidData)
+            // const bestBidResponse = await fetch(`${base_url}/api/v1/get_best_bid?ticker=${ticker}`)
+            // const bestBidData = await bestBidResponse.json()
+            // setBestBid(bestBidData)
 
-            const bestAskResponse = await fetch(`${base_url}/api/v1/get_best_ask?ticker=${ticker}`)
-            const bestAskData = await bestAskResponse.json()
-            setBestAsk(bestAskData)
+            // const bestAskResponse = await fetch(`${base_url}/api/v1/get_best_ask?ticker=${ticker}`)
+            // const bestAskData = await bestAskResponse.json()
+            // setBestAsk(bestAskData)
 
-            const midpriceResponse = await fetch(`${base_url}/api/v1/get_midprice?ticker=${ticker}`)
-            const midpriceData = await midpriceResponse.json()
-            setMidprice(midpriceData)
+            // const midpriceResponse = await fetch(`${base_url}/api/v1/get_midprice?ticker=${ticker}`)
+            // const midpriceData = await midpriceResponse.json()
+            // setMidprice(midpriceData)
         }
         const interval = setInterval(fetchData, 1000)
 
@@ -56,6 +61,44 @@ const TableComponent = ({ ticker }) => {
 
     return (
         <div>
+            <h2>Latest Trade</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Price</th>
+                        <th>Size</th>
+                        <th>Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{latestTrade.price}</td>
+                        <td>{latestTrade.size}</td>
+                        <td>{latestTrade.time}</td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2>Agents</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Agent</th>
+                        <th>Assets</th>
+                        <th>Net Worth</th>
+                        <th>Orders</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {agents.map((agent, index) => (
+                        <tr key={index}>
+                            <td>{agent.name}</td>
+                            <td>{JSON.stringify(agent.assets)}</td>
+                            <td>{agent.cash}</td>
+                            {/* <td>{JSON.stringify(agent._transactions)}</td> */}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
             {/* <table>
                 <thead>
                     <tr>
@@ -70,7 +113,7 @@ const TableComponent = ({ ticker }) => {
                     ))}
                 </tbody>
             </table> */}
-                        <h2>Order Book</h2>
+                        {/* <h2>Order Book</h2>
             <table>
                 <thead>
                     <tr>
@@ -166,7 +209,7 @@ const TableComponent = ({ ticker }) => {
                         <td>{JSON.stringify(midprice)}</td>
                     </tr>
                 </tbody>
-            </table>
+            </table> */}
         </div>
     )
 }
