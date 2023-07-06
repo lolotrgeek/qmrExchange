ec522f93154f18d43d41595edaa47bcf5925fac0
lolotrgeek <jon@simplyenable.com>
2023-07-06 13:06:39 -0500

    feat: request agent holdings

--- source/Requests.py
+++ source/Requests.py
@@ -103,3 +103,6 @@ class Requests():
     
     async def add_cash(self, agent, amount):
         return await self.make_request('add_cash', {'agent': agent, 'amount': amount}, self.requester)
+    
+    async def get_agents_holding(self, ticker):
+        return await self.make_request('get_agents_holding', {'ticker': ticker}, self.requester)
